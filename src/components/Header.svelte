4ad4038a20a88d4d415a4952bf18de7154c0829b
Jochen Stierberger <jochen@stierberger.com>
2023-11-03 08:26:02 -0500

    change wording and margin-top

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,15 +4,15 @@
 <header>
 	<h1>hey, I'm Jochen</h1>
 	<p>
-		I'm a passionate Software Engineer, tinkerer, and pizza nerd. I work at nvisia, where I build
-		cutting-edge IoT applications. I grew up in a tiny town in Germany, had the privilege of working
-		on three different continents and for now settled in Chicago, IL.
+		I'm a Software Engineer at nvisia, where I build cutting-edge IoT applications.<br /> I grew up in
+		a tiny town in Germany, had the privilege of working on three different continents and for now settled
+		in Chicago, IL.
 	</p>
 </header>
 
 <style>
 	header {
-		margin: 6rem 0;
+		margin-top: 8rem;
 	}
 
 	h1 {
