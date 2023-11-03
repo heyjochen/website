05061951b5482c149fa17f2c65c03443b163b541
Jochen Stierberger <jochen@stierberger.com>
2023-11-03 16:06:04 -0500

    uncomment nav entries

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
+	// const nav = [
+	// 	{
+	// 		name: 'Home',
+	// 		link: '/'
+	// 	},
+	// 	{ name: 'Notes', link: 'https://notes.stierberger.com' }
+	// ];
 </script>
 
 <header>
+	<!-- <nav> -->
+	<!-- 	<ul> -->
+	<!-- 		{#each nav as entry} -->
+	<!-- 			<li> -->
+	<!-- 				<a href={entry.link} target="_blank"> -->
+	<!-- 					{entry.name} -->
+	<!-- 				</a> -->
+	<!-- 			</li> -->
+	<!-- 		{/each} -->
+	<!-- 	</ul> -->
+	<!-- </nav> -->
 	<h1>hey, I'm Jochen</h1>
 	<p>
 		I'm a Software Engineer at nvisia, where I build cutting-edge IoT applications.<br /> I grew up in
@@ -12,8 +30,27 @@
 
 <style>
 	header {
-		margin-top: 8rem;
+		margin-top: 6rem;
 	}
+
+	/* ul { */
+	/* 	display: flex; */
+	/* 	justify-content: flex-end; */
+	/* 	gap: 0.75rem; */
+	/* 	margin-bottom: 4rem; */
+	/* } */
+	/**/
+	/* li { */
+	/* 	list-style-type: none; */
+	/* } */
+	/**/
+	/* a { */
+	/* 	color: var(--color-primary-100); */
+	/* } */
+	/**/
+	/* a:hover { */
+	/* 	color: var(--color-text); */
+	/* } */
 
 	h1 {
 		font-size: 1.5rem;
