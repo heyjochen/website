ca947a9386ae8b901627bdf26d026a88a6e43a68
Jochen Stierberger <jochen@stierberger.com>
2023-11-03 08:24:01 -0500

    add svg icon to note

--- src/components/Notes.svelte
+++ src/components/Notes.svelte
@@ -34,9 +34,17 @@
 		{#each notes as note}
 			<li>
 				<a href={note.href} target="_blank">
-					<span class="title">{note.title}</span>
-					<!-- <p>{note.desc}</p> -->
-					<span class="tags">#{note.tags}</span>
+					<div class="title-container">
+						<span class="title">{note.title}</span>
+						<!-- <p>{note.desc}</p> -->
+						<span class="tags">#{note.tags}</span>
+					</div>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M5.615 20q-.69 0-1.152-.462Q4 19.075 4 18.385V5.615q0-.69.463-1.152Q4.925 4 5.615 4h5.616v1H5.615q-.23 0-.423.192Q5 5.385 5 5.615v12.77q0 .23.192.423q.193.192.423.192h12.77q.23 0 .423-.192q.192-.193.192-.423v-5.616h1v5.616q0 .69-.462 1.152q-.463.463-1.153.463H5.615Zm4.123-5.03l-.707-.708L18.292 5H14V4h6v6h-1V5.708l-9.262 9.261Z"
+						/></svg
+					>
 				</a>
 			</li>
 		{/each}
@@ -58,8 +66,8 @@
 	a {
 		background-color: var(--color-primary-700);
 		display: flex;
-		gap: 0.25rem;
-		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
 		cursor: pointer;
 		padding: 1rem;
 		border: 1px solid var(--color-primary-600);
@@ -70,6 +78,12 @@
 
 	a:hover {
 		border: 1px solid var(--color-primary-600);
+	}
+
+	.title-container {
+		display: flex;
+		gap: 0.25rem;
+		flex-direction: column;
 	}
 
 	.title {
