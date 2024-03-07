bfb032501c366a9c02f9da867d55837c317a8217
Jochen Stierberger <jochen@stierberger.com>
2024-03-06 20:18:02 -0600

    create dynamic route

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -14,10 +14,16 @@
 	<ul class="posts">
 		{#each data.posts as post}
 			<li class="post">
-				<a href={post.slug} class="title">{post.title}</a>
+				<a href={`/blog/${post.slug}`} class="title">{post.title}</a>
+				<!-- Updated link -->
 				<p class="date">{formatDate(post.date)}</p>
 				<p class="description">{post.description}</p>
 			</li>
 		{/each}
 	</ul>
 </section>
+
+<style>
+	/* Styles remain the same */
+</style>
+
