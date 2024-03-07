333a8cebe74704dff95272c2f71cdd434a1e84f3
Jochen Stierberger <jochen@stierberger.com>
2024-03-06 20:42:25 -0600

    add Blog component

---
+++ src/components/Blog.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+	// import * as config from '$lib/config';
+
+	export let data;
+</script>
+
+<ul class="posts">
+	{#each data.posts as post}
+		<li class="post">
+			<a href={`/blog/${post.slug}`} class="title">{post.title}</a>
+			<!-- Updated link -->
+			<p class="date">{formatDate(post.date)}</p>
+			<p class="description">{post.description}</p>
+		</li>
+	{/each}
+</ul>
