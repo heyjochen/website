3235bc4d7294caf5ae881f311c187b25b598eeee
Jochen Stierberger <jochen@stierberger.com>
2024-03-06 18:29:23 -0600

    add a global config file

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from '$app/environment'
+
+export const title = 'Jochen Stierberger'
+export const description = 'Personal Website'
+export const url = dev ? 'http://localhost:5173/' : 'https://stierberger.com/'
+
